1444d99441c185797d44c3b4cd405b19b091e700
Dziamid <dziamid@gmail.com>
2010-12-24 17:51:35 +0200

    init

---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    marcel
+ * @subpackage form
+ * @author     Dziamid 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
