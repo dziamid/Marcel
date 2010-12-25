f412e8e2358cfe708667a1615c730c4734f3cc63
Dziamid <dziamid@gmail.com>
2010-12-25 19:31:18 +0200

    added menu_group schema

---
+++ lib/filter/doctrine/MenuGroupFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * MenuGroup filter form.
+ *
+ * @package    marcel
+ * @subpackage filter
+ * @author     Dziamid
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class MenuGroupFormFilter extends BaseMenuGroupFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/MenuGroupForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * MenuGroup form.
+ *
+ * @package    marcel
+ * @subpackage form
+ * @author     Dziamid
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class MenuGroupForm extends BaseMenuGroupForm
+{
+  public function configure()
+  {
+  }
+}
