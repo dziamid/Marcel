054bd10d22ee0b6c74505e3c961da04f81f2a2bd
Dziamid <dziamid@gmail.com>
2010-12-30 15:15:39 +0200

    fix bug with menu item link

--- apps/frontend/modules/menu_item/templates/_list.php
+++ apps/frontend/modules/menu_item/templates/_list.php
@@ -16,10 +16,8 @@ $(function() {
     <div id='<?php echo sprintf('tabs-%s',$k) ?>' class='<?php echo $menu_group->getSlug() ?>'>
       <?php foreach ($menu_group->getItems() as $menu_item): ?>
         <div class='menu_item'>
-          <?php if (sfConfig::get('app_use_ajax',false)): ?>
-            <?php echo link_to($menu_item->getName(), 'bill_select', array('sf_subject'=>$open_bill,'menu_item_id'=>$menu_item->getId())) ?>
-          <?php elseif ($open_bill->isNotNull()): ?>
-            <?php echo link_to($menu_item->getName(), 'bill_select', array('sf_subject'=>$open_bill,'menu_item_id'=>$menu_item->getId()),array('method'=>'post')) ?>
+          <?php if ($open_bill->isNotNull()): ?>
+            <?php echo link_to($menu_item->getName(), 'bill_select', array('sf_subject'=>$open_bill,'menu_item_id'=>$menu_item->getId()), sfConfig::get('app_use_ajax',false) ? array() : array('method'=>'post')) ?>
           <?php else: ?>
             <?php echo $menu_item->getName() ?>
           <?php endif; ?>
