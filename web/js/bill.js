2277f12c369f1411196c376fa90c70ec4bd41a76
Dziamid <dziamid@gmail.com>
2011-01-02 16:45:43 +0200

    fixed ajaxs bug: check if a is clicked

--- web/js/bill.js
+++ web/js/bill.js
@@ -1,17 +1,24 @@
 $(document).ready(function(){
   //select links in menu
-  $('#menulist').bind('click',function(e){
+  $('#menulist a').bind('click',function(e){
     e.preventDefault();
-    $.post($(e.target).attr('href'), function(data) {
-      $('#openbill table.bill').html(data);
-    });
+    var target = $(e.target).closest('a');
+    if (target.length)
+    {
+      $.post(target.attr('href'), function(data) {
+        $('#openbill table.bill').html(data);
+      });
+    }
   });
   //delete links in bill
   $('#openbill table.bill').bind('click',function(e){
     e.preventDefault();
-    var url = $(e.target).parents('a').attr('href');
-    $.post(url, function(data) {
-      $('#openbill table.bill').html(data);
-    });
+    var target = $(e.target).closest('a');
+    if (target.length)
+    {
+      $.post(target.attr('href'), function(data) {
+        $('#openbill table.bill').html(data);
+      });      
+    }
   });
 });
