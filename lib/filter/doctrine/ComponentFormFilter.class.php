0c8e08ffb63e02156195054e1503bbca83f1c55b
Dziamid <dziamid@gmail.com>
2011-01-17 22:23:26 +0200

    added component schema

---
+++ lib/filter/doctrine/ComponentFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Component filter form.
+ *
+ * @package    marcel
+ * @subpackage filter
+ * @author     Dziamid
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ComponentFormFilter extends BaseComponentFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/ComponentForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Component form.
+ *
+ * @package    marcel
+ * @subpackage form
+ * @author     Dziamid
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ComponentForm extends BaseComponentForm
+{
+  public function configure()
+  {
+  }
+}
