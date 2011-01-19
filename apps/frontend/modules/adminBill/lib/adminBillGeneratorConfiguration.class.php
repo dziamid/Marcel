bd98600ad85b1468d36d4bdc06dc30a71cb828b7
Dziamid <dziamid@gmail.com>
2011-01-19 19:57:43 +0200

    added admin bill module

---
+++ apps/frontend/modules/adminBill/lib/adminBillGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * adminBill module configuration.
+ *
+ * @package    marcel
+ * @subpackage adminBill
+ * @author     Dziamid
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class adminBillGeneratorConfiguration extends BaseAdminBillGeneratorConfiguration
+{
+}
---
+++ apps/frontend/modules/adminBill/lib/adminBillGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * adminBill module helper.
+ *
+ * @package    marcel
+ * @subpackage adminBill
+ * @author     Dziamid
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class adminBillGeneratorHelper extends BaseAdminBillGeneratorHelper
+{
+}
