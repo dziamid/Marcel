6dbd17929923c8ea2930aefdbad73219738b151a
Dziamid <dziamid@gmail.com>
2011-03-23 21:51:04 +0200

    turned off sfDoctrineGuardPlugin

--- apps/frontend/modules/home/templates/indexSuccess.php
+++
@@ -1,9 +0,0 @@
-<h1>Вход</h1>
-<div id="signin" class="area">
-  <?php if (!$sf_user->isAuthenticated()): ?>
-    <?php include_component('sfGuardAuth', 'signin_form') ?>
-  <?php else: ?>
-    <p> Здравствуйте, <?php echo $sf_user->getUsername() ?></p>
-    <p><?php echo link_to('Выход', '@sf_guard_signout') ?></p>
-  <?php endif; ?>
-</div>
