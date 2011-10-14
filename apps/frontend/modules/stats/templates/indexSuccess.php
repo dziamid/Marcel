3de12547f8c88ad7724758fd4f2fcf495474b922
Dziamid <dziamid@gmail.com>
2011-10-14 11:42:28 +0300

    Fixed css

--- apps/frontend/modules/stats/templates/indexSuccess.php
+++ apps/frontend/modules/stats/templates/indexSuccess.php
@@ -1,27 +1,35 @@
-<h1>Items List</h1>
+<?php use_stylesheet('/sfDoctrinePlugin/css/global.css', 'first') ?> 
+<?php use_stylesheet('/sfDoctrinePlugin/css/default.css', 'first') ?> 
 
-<table>
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Price</th>
-      <th>Total quantity</th>
-      <th>Total sum</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($items as $item): ?>
-    <tr>
-      <td><?php echo $item['name'] ?></td>
-      <td><?php echo $item['price'] ?></td>
-      <td><?php echo $item['total_quantity'] ?></td>
-      <td><?php echo $item['total_sum'] ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
-
-  <a href="<?php echo url_for('stats_new') ?>">New</a>
+<div id="sf_admin_container">
   
+  <h1>Статистика по наименованию</h1>
+  
+  <div id="sf_admin_bar">
     <?php include_partial('stats/filters', array('form' => $filters)) ?>
-
+  </div>
+  
+  <div id="sf_admin_content">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Total quantity</th>
+          <th>Total sum</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($items as $i => $item): $odd = fmod(++$i, 2) ? 'odd' : 'even' ?>
+        <tr class="sf_admin_row <?php echo $odd ?>">
+          <td class="sf_admin_text"><?php echo $item['name'] ?></td>
+          <td><?php echo $item['price'] ?></td>
+          <td><?php echo $item['total_quantity'] ?></td>
+          <td><?php echo $item['total_sum'] ?></td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>        
+  </div>
+  
+</div>
