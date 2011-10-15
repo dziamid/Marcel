b22f50786f5a97e5c038f712c613ff8d5833c359
Dziamid <dziamid@gmail.com>
2011-10-15 18:08:00 +0300

    cleaned up

--- apps/frontend/modules/stats/actions/actions.class.php
+++ apps/frontend/modules/stats/actions/actions.class.php
@@ -45,54 +45,6 @@ class statsActions extends sfActions
     $this->item = $this->getRoute()->getObject();
   }
 
-  public function executeNew(sfWebRequest $request)
-  {
-    $this->form = new ItemForm();
-  }
-
-  public function executeCreate(sfWebRequest $request)
-  {
-    $this->form = new ItemForm();
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('new');
-  }
-
-  public function executeEdit(sfWebRequest $request)
-  {
-    $this->form = new ItemForm($this->getRoute()->getObject());
-  }
-
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->form = new ItemForm($this->getRoute()->getObject());
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');
-  }
-
-  public function executeDelete(sfWebRequest $request)
-  {
-    $request->checkCSRFProtection();
-
-    $this->getRoute()->getObject()->delete();
-
-    $this->redirect('@stats');
-  }
-
-  protected function processForm(sfWebRequest $request, sfForm $form)
-  {
-    $form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $item = $form->save();
-
-      $this->redirect('@stats_edit?id='.$item->getId());
-    }
-  }
-  
   protected function getFilters()
   {
     return $this->getUser()->getAttribute('stats.filters', array(), 'admin_module');
